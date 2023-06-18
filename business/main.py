a1cc696c2e5f7f0d4c992b374682413ae460140a
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-06-18 11:08:58 -0300

    feat: handle http errors in business layer

--- business/main.py
+++ business/main.py
@@ -13,16 +13,23 @@ business_app = Flask(__name__)
 def create_posts() -> None:
     """Handle /posts HTTP and call with RMI the "save_posts_to_database" routine"""
 
-    posts = request.data
+    try:
+        posts = request.data
 
-    if not posts:
-        return jsonify("Posts not provided"), 400
+        if not posts:
+            return jsonify("Posts not provided"), 400
 
-    RpcConnection(Env.RPC_SERVER_HOST, Env.RPC_SERVER_PORT).bind().call_procedure(
-        "save_posts_to_database",
-        posts,
-    )
+        RpcConnection(Env.RPC_SERVER_HOST, Env.RPC_SERVER_PORT).bind().call_procedure(
+            "save_posts_to_database",
+            posts,
+        )
+
+        return jsonify("Posts successfully created"), 200
+    except:
+        return jsonify("Server error"), 500
 
 
 if __name__ == "__main__":
+    print(f"Starting HTTP server at {Env.HTTP_SERVER_HOST}:{Env.HTTP_SERVER_PORT}")
+
     business_app.run(host=Env.HTTP_SERVER_HOST, port=Env.HTTP_SERVER_PORT, debug=True)

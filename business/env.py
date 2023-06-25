7852a1147783f4ba19e6d64220ff722bd0d11b55
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-06-25 16:11:49 -0300

    feat: add posts input file and add posts filepath in business env

--- business/env.py
+++ business/env.py
@@ -1,9 +1,13 @@
 """This module defines the Env class"""
 
+from os.path import dirname, join
+
 
 class Env:
     BUSINESS_HTTP_SERVER_HOST = "business"
     BUSINESS_HTTP_SERVER_PORT = 3001
+
+    POSTS_INPUT_FILEPATH = join(dirname(__file__), "input", "posts.json")
 
     RPC_SERVER_HOST = "data"
     RPC_SERVER_PORT = 3002

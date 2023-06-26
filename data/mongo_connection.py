3d87eecadfca04f6b0b47c533333958d4dc80841
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-06-26 20:42:20 -0300

    refactor: add host and database props to MongoConnection

--- data/mongo_connection.py
+++ data/mongo_connection.py
@@ -6,12 +6,16 @@ from env import Env
 
 
 class MongoConnection:
+    host: str = None
+    database_name: str = None
+
     _client: MongoClient = None
 
-    def __init__(self) -> None:
-        self._client = MongoClient(
-            f"mongodb://{Env.MONGO_HOST}:{Env.MONGO_PORT}/{Env.MONGO_DATABASE_NAME}"
-        )
+    def __init__(self, host: str, database_name: str) -> None:
+        self.host = host
+        self.database_name = database_name
+
+        self._client = MongoClient(f"mongodb://{host}/{database_name}")
 
     def insert_many(self, collection: str, data: list) -> None:
-        self._client[Env.MONGO_DATABASE_NAME][collection].insert_many(data)
+        self._client[self.database_name][collection].insert_many(data)

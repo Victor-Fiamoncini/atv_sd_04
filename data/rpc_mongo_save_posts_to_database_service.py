7d9f3348eb38d980f510b616a2b8df9067bedf15
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-06-26 20:42:41 -0300

    feat: support multiple connections in RpcMongoSavePostsToDatabaseService

--- data/rpc_mongo_save_posts_to_database_service.py
+++ data/rpc_mongo_save_posts_to_database_service.py
@@ -1,5 +1,7 @@
 """This module defines the RpcMongoSavePostsToDatabaseService class"""
 
+from typing import List
+
 import rpyc
 
 from mongo_connection import MongoConnection
@@ -8,15 +10,16 @@ from save_posts_to_database_service import SavePostsToDatabaseService
 
 @rpyc.service
 class RpcMongoSavePostsToDatabaseService(SavePostsToDatabaseService, rpyc.Service):
-    mongo_connection: MongoConnection = None
+    mongo_connections: List[MongoConnection] = []
 
-    def __init__(self, mongo_connection: MongoConnection) -> None:
+    def __init__(self, mongo_connections: List[MongoConnection]) -> None:
         super().__init__()
 
-        self.mongo_connection = mongo_connection
+        self.mongo_connections = mongo_connections
 
     @rpyc.exposed
     def save_posts_to_database(self, *args) -> None:
         posts = args[0][0]
 
-        self.mongo_connection.insert_many("posts", posts)
+        for mongo_connection in self.mongo_connections:
+            mongo_connection.insert_many("posts", posts)

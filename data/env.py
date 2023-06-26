481ef0c639cefe7cd638f15743b545e83cfe198d
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-06-26 20:41:48 -0300

    feat: add mongodb instances hosts to data env

--- data/env.py
+++ data/env.py
@@ -2,9 +2,14 @@
 
 
 class Env:
-    MONGO_DATABASE_NAME = "atv_sd_04_mongo"
-    MONGO_HOST = "mongo_db"
-    MONGO_PORT = 27017
+    MONGO_DATABASE_NAME_01 = "atv_sd_04_mongo_db_01"
+    MONGO_HOST_01 = "mongo_db_01"
+
+    MONGO_DATABASE_NAME_02 = "atv_sd_04_mongo_db_02"
+    MONGO_HOST_02 = "mongo_db_02"
+
+    MONGO_DATABASE_NAME_03 = "atv_sd_04_mongo_db_03"
+    MONGO_HOST_03 = "mongo_db_03"
 
     RPC_SERVER_HOST = "data"
     RPC_SERVER_PORT = 3002

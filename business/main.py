510c9fb592f774743e89ec8ac69aa45e9f2af923
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-07-01 16:13:27 -0300

    feat: add GET /posts to business layer

--- business/main.py
+++ business/main.py
@@ -20,6 +20,34 @@ def health_check() -> Response:
     return jsonify("Business server is alive"), 200
 
 
+@business_app.get("/posts")
+def get_posts() -> Response:
+    """Handle /posts HTTP and call with RMI the "get_posts_from_database" routine"""
+
+    try:
+        rpc_connection = RpcConnection(Env.RPC_SERVER_HOST, Env.RPC_SERVER_PORT)
+        database_posts = rpc_connection.bind().call_procedure(
+            "get_posts_from_database", None
+        )
+
+        parsed_posts = [
+            {
+                "document_id": str(post.get("_id", "")),
+                "userId": post.get("userId", None),
+                "id": post.get("id", None),
+                "title": post.get("title", None),
+                "body": post.get("body", None),
+            }
+            for post in database_posts
+        ]
+
+        return jsonify(parsed_posts), 200
+    except GenericException as generic_exception:
+        return jsonify(f"An RPC error occurred: {generic_exception}"), 400
+    except Exception as exception:
+        return jsonify(f"Internal server error: {exception}"), 500
+
+
 @business_app.post("/posts")
 def create_posts() -> Response:
     """Handle /posts HTTP and call with RMI the "save_posts_to_database" routine"""
@@ -33,7 +61,7 @@ def create_posts() -> Response:
             parsed_posts_list,
         )
 
-        return jsonify("Posts successfully created"), 200
+        return jsonify("Posts successfully created"), 201
     except GenericException as generic_exception:
         return jsonify(f"An RPC error occurred: {generic_exception}"), 400
     except Exception as exception:
